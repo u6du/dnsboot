de1d9074cad8205bd406d15e63cb1c9860c7d05b
- <->
2019-07-02 12:22:07 +0800

    .

--- dnsboot.go
+++ dnsboot.go
@@ -5,19 +5,22 @@ import (
 	"github.com/u6du/dns"
 )
 
-var HostBootDefault = "6du.host"
+var HostBootDefault = "ip.6du.host"
+var HostBootPath = "dns/host/boot/"
 
-func main() {
-	hostPath := "dns/host/boot/"
+func BootLi(net string, dns *dns.Dns) {
 
-	v4host := config.File.OneLine(hostPath+"4", "ip4."+HostBootDefault)
+	v4host := config.File.OneLine(HostBootPath+net, net+"."+HostBootDefault)
 
-	v4txt := dns.V4.Txt(v4host, func(s string) bool {
-		println("ip4  ", s)
+	v4txt := dns.Txt(v4host, func(s string) bool {
+		println("ipv", net, "  ", s)
 		return true
 	})
+	println(v4txt)
+}
 
+func main() {
+	BootLi("4", &dns.V4)
 	//	var HostTestTxt = config.File.OneLine("dns/host/test/txt", "g.cn")
 
-	println(v4txt)
 }
